040a655dc8b3d0fcc7484147b91225f6de2b55bc
yehiax <yehiagamaleg@gmail.com>
2022-11-11 20:35:02 +0200

    remove feedspage and create home component instead

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FeedspageComponent } from './feedspage/feedspage.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
-    FeedspageComponent
+
+  
+    HomeComponent
   ],
   imports: [
     CommonModule
--- src/app/modules/home/home/home.component.spec.ts
+++ src/app/modules/home/home/home.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FeedspageComponent } from './feedspage.component';
+import { HomeComponent } from './home.component';
 
-describe('FeedspageComponent', () => {
-  let component: FeedspageComponent;
-  let fixture: ComponentFixture<FeedspageComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FeedspageComponent ]
+      declarations: [ HomeComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(FeedspageComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
