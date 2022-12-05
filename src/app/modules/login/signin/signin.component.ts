b97491b9159174b507808f4eb174d68f6f76bdf5
yehiax <yehiagamaleg@gmail.com>
2022-12-05 16:27:36 +0200

    create login home components

---
+++ src/app/modules/login/signin/signin.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+
+  constructor(private auth: AuthService) { }
+
+  ngOnInit(): void {
+    this.auth.getToken()
+
+  }
+
+}
